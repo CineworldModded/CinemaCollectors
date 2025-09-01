d7c4125c6444b28f014ca9cb7b97e3b5776a4d7b
CineworldModded <billdenbroughauthor@gmail.com>
2025-09-01 15:25:11 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,7 @@
+import { signUp, signIn } from './auth.js';
+
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+
+window.signUp = () => signUp(emailInput.value, passwordInput.value);
+window.signIn = () => signIn(emailInput.value, passwordInput.value);
