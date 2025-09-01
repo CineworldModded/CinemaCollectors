1a0489bc36a41045782fa88a017c944d0d461309
CineworldModded <billdenbroughauthor@gmail.com>
2025-09-01 18:33:28 +0100

    Update login.js

--- login.js
+++ login.js
@@ -2,8 +2,7 @@ import { signUp, signIn } from './auth.js';
 
 const emailInput = document.getElementById("email");
 const passwordInput = document.getElementById("password");
-const resetBtn = document.getElementById('resetBtn');
 
 window.signUp = () => signUp(emailInput.value, passwordInput.value);
 window.signIn = () => signIn(emailInput.value, passwordInput.value);
-window.resetPassword = () => signIn();
+window.resetPassword = () => resetPassword(emailInput.value);
