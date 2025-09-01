ae2ed95fd20c04d079c1ff9013ea48ee43dcd1d0
CineworldModded <billdenbroughauthor@gmail.com>
2025-09-01 18:31:12 +0100

    Update login.js

--- login.js
+++ login.js
@@ -6,19 +6,4 @@ const resetBtn = document.getElementById('resetBtn');
 
 window.signUp = () => signUp(emailInput.value, passwordInput.value);
 window.signIn = () => signIn(emailInput.value, passwordInput.value);
-
-resetBtn.addEventListener('click', async () => {
-  const email = document.getElementById('email').value;
-
-  if (!email) return alert("Please enter your email first.");
-
-  const { data, error } = await supabase.auth.resetPasswordForEmail(email, {
-    redirectTo: 'https://cinema-collectors.vercel.app/login.html' // optional redirect after reset
-  });
-
-  if (error) {
-    alert("Error sending reset email: " + error.message);
-  } else {
-    alert("Check your email for the password reset link!");
-  }
-});
+window.resetPassword = () => signIn(emailInput.value);
